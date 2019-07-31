4af9bf88abb2c70d9065a7b0f181efc1c1c0f83f
mudrakpatel <mudrakpatel96@gmail.com>
2019-07-31 15:03:56 -0400

    useState hook with array

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 //import ClassCounter from './components/ClassCounter';
 //import HookCounter from './components/HookCounter';
 //import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
+//import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/*<ClassCounter/>*/}
       {/*<HookCounter/>*/}
       {/*<HookCounter2/>*/}
-      <HookCounter3/>
+      {/*<HookCounter3/>*/}
+      <HookCounter4/>
     </div>
   );
 }
